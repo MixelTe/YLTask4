d6848d1fcd0dbaa0af31e3ce683469ffb5e5d03c
Mike <63655075+MixelTe@users.noreply.github.com>
2022-02-20 13:23:02 +0300

    part 5

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMessageBox, QGroupBox, QHBoxLayout, QRadioButton
-from PyQt5.QtGui import QPixmap, QKeyEvent
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMessageBox, QGroupBox
+from PyQt5.QtWidgets import QHBoxLayout, QRadioButton, QLineEdit, QPushButton
+from PyQt5.QtGui import QPixmap, QKeyEvent, QFont
+from PyQt5.QtCore import Qt, QEvent
 
 
-def loadImg(ll: tuple[float, float], z: int, l: str, w=450, h=450):
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={ll[0]},{ll[1]}&l={l}&z={z}&size={w},{h}"
-    response = requests.get(map_request)
+def loadImg(ll: tuple[float, float], z: int, l: str, pt=None, w=450, h=450):
+    map_request = f"http://static-maps.yandex.ru/1.x/"
+    params = {
+        "ll": f"{ll[0]},{ll[1]}",
+        "l": l,
+        "z": z,
+        "size": f"{w},{h}",
+    }
+    if (pt):
+        params["pt"] = pt
+    response = requests.get(map_request, params=params)
 
     if not response:
         text = "Запрос:\n"
-        text += map_request + "\n"
+        text += response.url + "\n"
         text += f"Http статус: {response.status_code} ({response.reason})"
         msgbox = QMessageBox()
         msgbox.setWindowTitle("Ошибка выполнения запроса")
@@ -24,6 +33,25 @@ def loadImg(ll: tuple[float, float], z: int, l: str, w=450, h=450):
     return pixmap
 
 
+def getCoords(geocode):
+    geocoder_request = f"https://geocode-maps.yandex.ru/1.x/"
+    params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": geocode,
+        "format": "json",
+    }
+
+    response = requests.get(geocoder_request, params=params)
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        toponym_coodrinates = list(map(float, toponym_coodrinates.split(" ")))
+        return toponym_coodrinates
+    else:
+        return None
+
+
 class Form(QWidget):
     def __init__(self):
         super().__init__()
@@ -33,12 +61,16 @@ class Form(QWidget):
         self.setImg()
 
     def initUI(self):
-        self.setGeometry(300, 300, 482, 520)
+        self.setGeometry(300, 300, 482, 550)
         self.setWindowTitle('Большая задача по Maps API')
+        font = QFont()
+        font.setPointSize(10)
+        self.setFont(font)
 
         self.label = QLabel(self)
         self.label.move(16, 16)
         self.label.resize(450, 450)
+        self.label.setFocus()
 
         groupBox = QGroupBox(self)
         groupBox.setGeometry(10, 470, 461, 41)
@@ -51,20 +83,38 @@ class Form(QWidget):
         self.rb_map_map.setText("схема")
         self.rb_map_map.setChecked(True)
         self.rb_map_map.toggled.connect(self.setImg)
+        self.rb_map_map.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         horizontalLayout.addWidget(self.rb_map_map)
 
         self.rb_map_sat = QRadioButton(horizontalLayoutWidget)
         self.rb_map_sat.setText("спутник")
         self.rb_map_sat.toggled.connect(self.setImg)
+        self.rb_map_sat.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         horizontalLayout.addWidget(self.rb_map_sat)
 
         self.rb_map_hyb = QRadioButton(horizontalLayoutWidget)
         self.rb_map_hyb.setText("гибрид")
         self.rb_map_hyb.toggled.connect(self.setImg)
+        self.rb_map_hyb.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         horizontalLayout.addWidget(self.rb_map_hyb)
 
-    def keyPressEvent(self, e: QKeyEvent) -> None:
+        self.inp_search = QLineEdit(self)
+        self.inp_search.setGeometry(10, 520, 371, 21)
+        self.btn_search = QPushButton(self)
+        self.btn_search.setGeometry(394, 520, 71, 21)
+        self.btn_search.setText("Искать")
+        self.btn_search.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.btn_search.clicked.connect(self.search)
+
+    def mousePressEvent(self, e) -> None:
+        self.inp_search.clearFocus()
+
+    def keyReleaseEvent(self, e: QKeyEvent) -> None:
         super().keyPressEvent(e)
+        if (self.inp_search.hasFocus()):
+            if (e.key() == Qt.Key.Key_Enter or e.key() == Qt.Key.Key_Return):
+                self.search()
+            return
         if (e.key() == Qt.Key.Key_PageUp):
             self.z += 1
             self.z = min(self.z, 17)
@@ -90,14 +140,29 @@ class Form(QWidget):
             self.ll[1] = max(self.ll[1], -85)
             self.setImg()
 
-    def setImg(self):
-        mapType = "map"
+    def getMapType(self):
         if (self.rb_map_sat.isChecked()):
-            mapType = "sat"
+            return "sat"
         elif (self.rb_map_hyb.isChecked()):
-            mapType = "sat,skl"
-        self.pixmap = loadImg(self.ll, self.z, mapType)
+            return "sat,skl"
+        return "map"
+
+    def setImg(self, *arg, pt=None):
+        self.pixmap = loadImg(self.ll, self.z, self.getMapType(), pt)
         self.label.setPixmap(self.pixmap)
+
+    def search(self):
+        coords = getCoords(self.inp_search.text())
+        if (coords is None):
+            msgbox = QMessageBox()
+            msgbox.setWindowTitle("Поиск объекта")
+            msgbox.setIcon(QMessageBox.Icon.Information)
+            msgbox.setText("Не удалось найти объект.")
+            msgbox.exec()
+            return
+        self.ll = coords
+        self.inp_search.clearFocus()
+        self.setImg(pt=f"{','.join(map(str, coords))},pm2rdm")
 
 
 if __name__ == '__main__':
